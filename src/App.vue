ecfd107202ee6ea0a929978aae451dcf43c2e209
Iraldin <ludovic.chausiaux@gmail.com>
2023-03-29 15:20:34 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="container">
+    <h3>Ma fiche perso</h3>
+    <div class="row">
+      <div class="col-6">
+        <div class="mb-3">
+          <label class="form-label" for="lastname">Nom</label>
+          <input v-model="lastname" class="form-control" type="text">
+        </div>
+      </div>
+      <div class="col-6">
+        <div class="mb-3">
+          <label class="form-label" for="firstname">Prénom</label>
+          <input class="form-control" type="text">
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-6">
+        <div class="mb-3">
+          <label class="form-label" for="age">Âge</label>
+          <input class="form-control" type="number">
+        </div>
+      </div>
+      <div class="col-6">
+        <div class="mb-3">
+          <label class="form-label" for="gender">Genre</label>
+          <select class="form-control">
+            <option selected disabled>Selectionner une valeur</option>
+            <option value="male">Homme</option>
+            <option value="female">Femme</option>
+            <option value="other">Autre</option>
+          </select>
+        </div>
+      </div>
+    </div>
+    <hr>
+    {{ lastnameDisplay }}
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      lastname: '',
+      lastnameDisplay: '',
+    };
+  },
+  methods: {},
+  computed: {}
+}
+</script>
---
+++ src/App2.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="container">
+    <p>{{ text }}</p>
+    <div class="d-flex">
+      <p class="mx-2">J'ai {{ age }} ans</p>
+      <button class="mx-2" @click="age++">Anniversaire</button>
+    </div>
+    <p>
+      Je suis : {{ gender }}
+    </p>
+    <div class="d-flex">
+      <button class="btn btn-primary" @click="updateSex('male')">Un homme</button>
+      <button class="btn btn-primary" @click="updateSex('female')">Une femme</button>
+      <button class="btn btn-primary" @click="updateSex('other')">Autres</button>
+    </div>
+    <div class="row mt-3">
+      <div class="form-group">
+        <label for="message">Message : </label>
+        <input v-model="message" class="form-control" type="text">
+        <p class="mt-2">Je dis : {{ message }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      text: "Hello World",
+      age: 27,
+      pets: ['Irion', 'Spooky'],
+      sex: null,
+      message: 'bonjour',
+    };
+  },
+  methods: {
+    updateSex(gender) {
+      this.sex = gender;
+    },
+  },
+  computed: {
+    gender() {
+      if (this.sex === 'male') {
+        return 'un homme';
+      } else if (this.sex === 'female') {
+        return 'une femme';
+      }
+      return 'autre';
+    }
+  }
+}
+</script>
+
+<style>
+.d-flex {
+  display: flex;
+}
+
+.mx-2 {
+  margin: auto 2em;
+}
+</style>
